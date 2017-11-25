adc0209ec36e282a8829ddc84a15d7f3e0320eb6
Tuan Vo <automatwon@gmail.com>
2017-11-25 00:38:32 +0000

    Store Google refresh tokens and add /auth/consent route to get new refresh token

--- server/auth.js
+++ server/auth.js
@@ -1,12 +1,41 @@
 import express from 'express';
+import fs from 'fs';
+import jsonfile from 'jsonfile';
 import passport from 'passport';
 import {GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, GOOGLE_REDIRECT_URI} from './secrets';
 import {Strategy as GoogleStrategy} from 'passport-google-oauth20';
 
 const router = express.Router();
 
-console.log(GOOGLE_CLIENT_ID);
-console.log(GOOGLE_CLIENT_ID);
+// TODO: Persist to Memcached instead of flat file.
+class GoogleRefreshTokenStore {
+  constructor() {
+    this.filePath = '/tmp/data.json';
+  }
+
+  set(id, refreshToken) {
+    const obj = fs.existsSync(this.filePath) ? jsonfile.readFileSync(this.filePath) : {};
+    obj[id] = refreshToken;
+    jsonfile.writeFileSync(this.filePath, obj);
+  }
+
+  get(id) {
+    if(!fs.existsSync(this.filePath)) {
+      return null;
+    }
+    const obj = jsonfile.readFileSync(this.filePath);
+    return obj[id];
+  }
+
+  has(id) {
+    if(!fs.existsSync(this.filePath)) {
+      return false;
+    }
+    const obj = jsonfile.readFileSync(this.filePath);
+    return !!obj[id];
+  }
+}
+const googleRefreshTokenStore = new GoogleRefreshTokenStore();
 
 passport.use(new GoogleStrategy({
     clientID: GOOGLE_CLIENT_ID,
@@ -14,7 +43,15 @@ passport.use(new GoogleStrategy({
     callbackURL: GOOGLE_REDIRECT_URI,
   },
   function(accessToken, refreshToken, profile, cb) {
-    const user = {googleId: profile.id}
+    const googleId = profile.id;
+    // Save refresh token.
+    if(refreshToken) {
+      googleRefreshTokenStore.set(googleId, refreshToken);
+    } else if (!googleRefreshTokenStore.has(googleId)) {
+      // Get new refresh token.
+       return cb(null, false);
+    }
+    const user = {id: googleId};
     return cb(null, user);
   }
 ));
@@ -35,7 +72,7 @@ const scopes = [
 ];
 
 router.get('/google',
-  passport.authenticate('google', { scope: scopes }), 
+  passport.authenticate('google', { scope: scopes , accessType: 'offline' }), 
   (req, res, next) => {
     if (req.query.return) {
       req.session.oauth2return = req.query.return;
@@ -44,6 +81,15 @@ router.get('/google',
   });
 
 
+// Consent for a new refresh token.
+router.get('/consent',
+  passport.authenticate('google', { scope: scopes , accessType: 'offline', prompt: 'consent'}), 
+  (req, res, next) => {
+    if (req.query.return) {
+      req.session.oauth2return = req.query.return;
+    }
+    next();
+  });
 
 // Handler for google auth callback route depends on dev-environment.
 const googleCallbackRouteHandler = (function(isDev) {
@@ -61,7 +107,7 @@ const googleCallbackRouteHandler = (function(isDev) {
 })(process.env.NODE_ENV == 'development');
 
 router.get('/google/callback',
-  passport.authenticate('google', { failureRedirect: '/login' }),
+  passport.authenticate('google', { failureRedirect: '/auth/consent' }),
   googleCallbackRouteHandler);
 
 
